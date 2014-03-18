3163ccf83b46f1b30c0ce70ebf9bcf71672efa1e
SkylerTyler1337 <tylernichols409@gmail.com>
2014-03-18 16:03:52 -0500

    Still working on the rest

---
+++ src/me/skylertyler/zealpvp/ZealPvP.java
@@ -0,0 +1,29 @@
+package me.skylertyler.zealpvp;
+
+import me.skylertyler.zealpvp.Listeners.ZealPVPListener;
+import me.skylertyler.zealpvp.commands.PvPCommand;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ZealPvP extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+	PluginDescriptionFile pdffile = this.getDescription();
+	PluginManager pm = getServer().getPluginManager();
+	System.out.println("[ZealPvP] "+pdffile.getVersion()+" is Enabled!");
+	//Listeners!
+	pm.registerEvents(new ZealPVPListener(this), this);
+	//Commands
+	getCommand("pvp").setExecutor(new PvPCommand(this));
+	}
+	
+	@Override
+	public void onDisable() {
+	PluginDescriptionFile pdffile = this.getDescription();
+	System.out.println("[ZealPvP] "+pdffile.getVersion()+" is Disabled!");
+	}
+
+}
